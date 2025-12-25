16e7b42b25e82a7993d8ce679232998c110a2644
zhamanko <slavko.lenko28.06.2016@gmail.com>
2025-12-25 19:01:16 +0200

    feat: remove commented-out routes for income and sale from Header and router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,8 +4,6 @@
         <div>
             <nav class="text-white flex gap-12">
                 <router-link to="/products" class="text-lg hover:scale-110 transition">Товари</router-link>
-                <!-- <router-link to="/income">Прихід</router-link>
-                <router-link to="/sale">Продаж</router-link> -->
                 <router-link to="/operation" class="text-lg hover:scale-110 transition">Операції</router-link>
                 <router-link to="/history" class="text-lg hover:scale-110 transition">Історія</router-link>
             </nav>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ProductsView from '../views/ProductsView.vue'
-// import IncomeView from '../views/IncomeView.vue'
-// import SaleView from '../views/SaleView.vue'
 import OperationView from '../views/OperationView.vue'
 import HistoryView from '../views/HistoryView.vue'
 
 const routes = [
   { path: '/', redirect: '/products' },
   { path: '/products', component: ProductsView },
-  // { path: '/income', component: IncomeView },
-  // { path: '/sale', component: SaleView },
   { path: '/history', component: HistoryView },
   { path: '/operation', component: OperationView }
 ]
